b1743e67ca9219ea274d6a691fb93797266e9ef5
Kari Mah <kari.anne.mah@gmail.com>
2014-03-17 12:42:04 -0700

    fix hold retrieval script

--- app.rb
+++ app.rb
@@ -12,27 +12,41 @@ end
 post '/' do
   @book = params[:book]
 
+  link_agent = Mechanize.new
+  link_page = link_agent.get('http://csul.iii.com/')
+  link_agent.page.forms[0].fields[1].value = @book
+  link_result = link_agent.page.forms[0].submit
+  @link_url = link_result.uri
+
   agent = Mechanize.new
   page = agent.get('http://sfpl.org')
-
   agent.page.forms[0].fields[1].value = @book
-
   result = agent.page.forms[0].submit
   homepage = "http://sflib1.sfpl.org"
 
-  book_links = []
-  result.links.each do |link|
-    if link.text.include? 'Is it available?'
-      substring = link.href
-      url = homepage + substring + '/'
-      book_links << url
-    end
-  end
-
   @books = {}
   @urls = {}
-  book_links.each do |url|
-    page = Nokogiri::HTML(open(url))
+
+  url = result.uri
+  page = Nokogiri::HTML(open(url))
+
+  if page.css('span.bibHolds').text == ""
+    book_links = []
+    result.links.each do |link|
+      if link.text.include? 'Is it available?'
+        substring = link.href
+        url = homepage + substring + '/'
+        book_links << url
+      end
+    end
+
+    book_links.each do |url|
+      page = Nokogiri::HTML(open(url))
+      title = page.css('td.bibInfoData').text.split("\n")[1]
+      @books[title] = page.css('span.bibHolds').text
+      @urls[title] = url
+    end
+  else
     title = page.css('td.bibInfoData').text.split("\n")[1]
     @books[title] = page.css('span.bibHolds').text
     @urls[title] = url
