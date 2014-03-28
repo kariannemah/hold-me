409a94181198ac7ea674497bf50fc0a09c86f8e4
Kari Mah <kari.anne.mah@gmail.com>
2014-03-28 14:17:19 -0700

    refactor via extractions into methods

--- app.rb
+++ app.rb
@@ -16,67 +16,60 @@ post '/' do
     agent = Mechanize.new
     page = agent.get(search_url)
     page.forms[0].fields[1].value = search_term
-    page.forms[0].submit.uri
+    page.forms[0].submit
   end
 
-  url = search_submitter(@book,'http://sfpl.org')
+  def get_holds(url)
+    page = Nokogiri::HTML(open(url))
+    title = page.css('td.bibInfoData').text.split("\n")[1].to_sym
+    @books[title] = {sfpl_url: url}
+    @books[title][:holds] = page.css('span.bibHolds').text
+  end
+
+  def overdrive_hold_info(page, dom_element)
+    /\d/.match(page.css('ul.copies-expand.tog-close.details-ul-exp').children[dom_element].text).to_s
+  end
+
+  def axis_hold_info(page, dom_element)
+    /\d/.match(page.css('div.ActionPanelInfo').children[dom_element].text).to_s
+  end
+
+  result = search_submitter(@book,'http://sfpl.org')
+  url = result.uri
 
   @link_plus_url = search_submitter(@book, 'http://csul.iii.com/')
 
   page = Nokogiri::HTML(open(url))
-  homepage = 'http://sflib1.sfpl.org'
   @books = {}
 
-  if page.css('span.bibHolds').text.empty?
-    book_links = []
-    result.links.each do |link|
-      if link.text.include? 'Is it available?'
-        substring = link.href
-        url = homepage + substring + '/'
-        book_links << url
-      end
-    end
-
-    book_links.each do |url|
-      page = Nokogiri::HTML(open(url))
-      title = page.css('td.bibInfoData').text.split("\n")[1].to_sym
-      @books[title] = {:sfpl_url => url}
-      @books[title][:holds] = page.css('span.bibHolds').text
-    end
-  else
-    title = page.css('td.bibInfoData').text.split("\n")[1].to_sym
-    @books[title] = {:sfpl_url => url}
-    @books[title][:holds] = page.css('span.bibHolds').text
-  end
-
-
+  (page.css('span.bibHolds').text == '') ?
+    result.links.keep_if { |link| link.text.include? 'Is it available?' }.map do |link|
+      url = 'http://sflib1.sfpl.org' + link.href + '/'
+      get_holds(url)
+    end :
+    get_holds(url)
 
   @books.each do |key,value|
-    if value.is_a? Hash
-      if key.to_s.split.include?('[electronic') || key.to_s.split.include?('(Online)')
-        ebook_page = Nokogiri::HTML(open(value[:sfpl_url]))
-        platform_url = ebook_page.css('table.bibLinks').first.children[1].children[0].children[1].attributes['href'].value
-        value[:ebook] = {:url => platform_url }
-        third_party_page =  Nokogiri::HTML(open(platform_url))
+    title = key.to_s.split
+    if title.include? ('[electronic')  || title.include?('(Online)')
+      page = Nokogiri::HTML(open(value[:sfpl_url]))
+      ebook_platform_url = page.css('table.bibLinks').first.children[1].children[0].children[1].attributes['href'].value
+      value[:ebook] = {url: ebook_platform_url}
+      ebook_page =  Nokogiri::HTML(open(ebook_platform_url))
 
-          if ! /overdrive/.match(platform_url).nil?
-            # overdrive
-            value[:ebook][:copies] = /\d/.match(third_party_page.css('ul.copies-expand.tog-close.details-ul-exp').children[2].text).to_s
-            value[:ebook][:holds] = /\d/.match(third_party_page.css('ul.copies-expand.tog-close.details-ul-exp').children[4].text).to_s
-          elsif ! /axis/.match(platform_url).nil?
-            # axis 360
-            value[:ebook][:copies] = /\d/.match(third_party_page.css('div.ActionPanelInfo').children[1].text).to_s
-            if /\d/.match(third_party_page.css('div.ActionPanelInfo').children[5].text).to_s == ''
-              value[:ebook][:holds] = '0'
-            else
-              value[:ebook][:holds] = /\d/.match(third_party_page.css('div.ActionPanelInfo').children[5].text).to_s
-            end
-          else
-            # other ebook platforms
-            value[:ebook][:holds] = ''
-            value[:ebook][:copies] = ''
-          end
-      end
+        if /overdrive/.match(ebook_platform_url)
+          value[:ebook][:copies] = overdrive_hold_info(ebook_page, 2)
+          value[:ebook][:holds] = overdrive_hold_info(ebook_page, 4)
+        elsif /axis/.match(ebook_platform_url)
+          value[:ebook][:copies] = axis_hold_info(ebook_page, 1)
+          (axis_hold_info(ebook_page, 5) == '') ?
+            value[:ebook][:holds] = '0' :
+            value[:ebook][:holds] = axis_hold_info(ebook_page, 5)
+        else
+          # other ebook platforms
+          value[:ebook][:holds] = ''
+          value[:ebook][:copies] = ''
+        end
     end
   end
 
